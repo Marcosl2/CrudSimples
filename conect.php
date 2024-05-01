dc93cc2863d9ea37a38cbe7baabf804f882e6a5f
Marcosl2 <lamed-4@hotmail.com>
2024-05-01 18:10:59 -0300

    Criação do bd

---
+++ conect.php
@@ -0,0 +1,15 @@
+<?php
+include '../config.php';
+/**
+ * 
+ * Conexao com o Banco de Dados. 
+ * 
+ * 
+ */
+$conn = mysqli_connect(HOST,USER,PASS,DBNAME);
+
+if ($conn->connect_error) {
+    die("falha na conexão: " . $conn->connect_error);
+  }
+  echo  "<h1 style='text-align:center; color:green;' >Conectado com Sucesso!!!</h1>";
+  ?>
